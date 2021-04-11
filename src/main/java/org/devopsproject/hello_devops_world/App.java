4750952e92f05722c785b0ee851f3129706ef27c
URITI KISHORE <URITIVENKATANAGAKISHORE>
2021-04-12 00:23:50 +0530

    added initial commit

---
+++ src/main/java/org/devopsproject/hello_devops_world/App.java
@@ -0,0 +1,13 @@
+package org.devopsproject.hello_devops_world;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
